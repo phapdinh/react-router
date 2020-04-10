30bf7105dd047bb6ada033ac3829c971ee306032
Phap <centralparkloop@yahoo.com>
2020-04-10 16:50:18 -0400

    convert class components into functional components

--- packages/react-router/modules/Switch.js
+++ packages/react-router/modules/Switch.js
@@ -9,40 +9,39 @@ import matchPath from "./matchPath.js";
 /**
  * The public API for rendering the first <Route> that matches.
  */
-class Switch extends React.Component {
-  render() {
-    return (
-      <RouterContext.Consumer>
-        {context => {
-          invariant(context, "You should not use <Switch> outside a <Router>");
+function Switch(props) {
+  this.props = props;
+  return (
+    <RouterContext.Consumer>
+      {context => {
+        invariant(context, "You should not use <Switch> outside a <Router>");
 
-          const location = this.props.location || context.location;
+        const location = this.props.location || context.location;
 
-          let element, match;
+        let element, match;
 
-          // We use React.Children.forEach instead of React.Children.toArray().find()
-          // here because toArray adds keys to all child elements and we do not want
-          // to trigger an unmount/remount for two <Route>s that render the same
-          // component at different URLs.
-          React.Children.forEach(this.props.children, child => {
-            if (match == null && React.isValidElement(child)) {
-              element = child;
+        // We use React.Children.forEach instead of React.Children.toArray().find()
+        // here because toArray adds keys to all child elements and we do not want
+        // to trigger an unmount/remount for two <Route>s that render the same
+        // component at different URLs.
+        React.Children.forEach(this.props.children, child => {
+          if (match == null && React.isValidElement(child)) {
+            element = child;
 
-              const path = child.props.path || child.props.from;
+            const path = child.props.path || child.props.from;
 
-              match = path
-                ? matchPath(location.pathname, { ...child.props, path })
-                : context.match;
-            }
-          });
+            match = path
+              ? matchPath(location.pathname, { ...child.props, path })
+              : context.match;
+          }
+        });
 
-          return match
-            ? React.cloneElement(element, { location, computedMatch: match })
-            : null;
-        }}
-      </RouterContext.Consumer>
-    );
-  }
+        return match
+          ? React.cloneElement(element, { location, computedMatch: match })
+          : null;
+      }}
+    </RouterContext.Consumer>
+  );
 }
 
 if (__DEV__) {
